2f682c67a248e0f183762efe6b2f2a9b42cd0034
sj-postlabs <seongjunpark@postlabs.io>
2023-06-30 17:15:54 +0900

    [23/06/30 17:15] feat : ChatInput 생성

---
+++ src/components/ChatInput.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styled from "styled-components";
+
+const ChatInput = ({}) => {
+  return (
+    <StyledChatInput>
+      <div>Something Here</div>
+    </StyledChatInput>
+  )
+};
+
+const StyledChatInput = styled.div`
+
+`;
+
+export default ChatInput;
