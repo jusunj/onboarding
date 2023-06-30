eb3333e39ebffa182db79b46aaa68cd7582864a3
sj-postlabs <seongjunpark@postlabs.io>
2023-06-30 19:07:28 +0900

    [23/06/30 19:06] feat : ChatInput 기본 구조

--- src/App2.js
+++ src/App2.js
@@ -1,12 +1,21 @@
 // import logo from "./logo.svg";
+import { useState } from "react";
 import "./App.css";
+import ChatInput from "./components/ChatInput";
 
 function App() {
+  const [message, setMessage] = useState(0);
+
   return (
     <div style={{display:"flex", justifyContent:"center", alignItems:"center"}}>
       <div style={{  width: "1200px", height: "300px", background:"#D9D9"}}>
         App2
 
+        <ChatInput
+          message={message}
+          messageMethod={setMessage}
+          hasInfoBox={false}
+        />
       </div>
     </div>
   );
--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,16 +1,85 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
+import { Body14, Body12 } from "../styles/typography";
 
-const ChatInput = ({}) => {
+const ChatInput = ({message, hasInfoBox}) => {
+  const [browserWidth, setBrowserWidth] = useState(document.documentElement.clientWidth);
+
+  const resizeTimer = useRef(null);
+  
+  useEffect(() => {
+    const handleResize = () => {
+      if (resizeTimer.current !== null) return;
+      resizeTimer.current = setTimeout(() => {
+        resizeTimer.current = null;
+        setBrowserWidth(window.innerWidth);
+      }, 200);
+    };
+  
+    window.addEventListener("resize", handleResize);
+  
+    return () => {
+      window.addEventListener("resize", handleResize);
+    };
+  }, [browserWidth]);
+
   return (
-    <StyledChatInput>
-      <div>Something Here</div>
+    <StyledChatInput browserWidth={browserWidth}>
+      <div className="chat-input-wrapper">
+        <div className="chat-input-core">
+          <textarea className="chat-input-text" id="chat-input-text" rows="6"></textarea>
+        </div>
+        <div className="chat-input-button">
+          <img src="inputIcons/InputButton.svg" style={{color: 'red', width: '36px', height: '36px'}}/>
+        </div>
+      </div>
+
+
+      {/* <div style={{display: "flex", alignItems: "center", margin: "8px 14px"}}>
+        <div style={{padding: "10px 12px"}}>
+          <textarea className="chat-core" type="text" rows="1" style={{display: "flex"}}/>
+        </div>
+        <div >
+          <img src="inputIcons/InputButton.svg" style={{color: 'red', width: '36px', height: '36px'}}/>
+        </div>
+      </div> */}
     </StyledChatInput>
   )
 };
 
 const StyledChatInput = styled.div`
+  .chat-input-wrapper {  
+    display: flex;
+    align-items: center;
+    background-color: white;
 
+    position: absolute;
+    bottom: 0px;
+    width: 100%;
+
+    padding: 7px 14px;
+    border-top: 1px solid #EBEBEB;
+  }
+
+  .chat-input-core {
+    width: ${(props)=>props.browserWidth - 100}px;
+    position: relative;
+  }
+  
+  .chat-input-text {
+    width: 100%;
+    height: auto;
+    border-radius: 10px;
+    padding: 10px 12px;
+    ${ Body14 }
+  }
+
+  .chat-input-button {
+    width: 36px;
+    height: 36px;
+    margin-left: 36px;
+    position: relative;
+  }
 `;
 
 export default ChatInput;
