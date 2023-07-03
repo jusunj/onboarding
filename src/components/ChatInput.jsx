42742999b17d8ad1b8e07004623117af70bb8b4c
sj-postlabs <seongjunpark@postlabs.io>
2023-07-03 14:02:43 +0900

    [23/07/03 14:02] feat : ChatInput 기본 구조 2

--- src/components/ChatInput.jsx
+++ src/components/ChatInput.jsx
@@ -1,12 +1,25 @@
 import React, { useState, useEffect, useRef } from "react";
 import styled from "styled-components";
 import { Body14, Body12 } from "../styles/typography";
+import ChatInput_send_button from "../svg/ChatInput_send_button";
 
-const ChatInput = ({message, hasInfoBox}) => {
-  const [browserWidth, setBrowserWidth] = useState(document.documentElement.clientWidth);
-
-  const resizeTimer = useRef(null);
+const ChatInput = (props) => {
+  // textarea 메시지 관련 코드
+  const [message, setMessage] = useState('');
+  const handleMessageChange = (e)=>{
+    setMessage(e.target.value);
+  };
   
+  // 버튼 활성화 관련 코드
+  const [buttonActivate, setButtonActivate] = useState(false);
+  useEffect(() => {
+    if (message.length > 0) setButtonActivate(true);
+    else setButtonActivate(false);
+  }, [message]);
+
+  // 브라우저 너비 관련 코드
+  const [browserWidth, setBrowserWidth] = useState(document.documentElement.clientWidth);
+  const resizeTimer = useRef(null);
   useEffect(() => {
     const handleResize = () => {
       if (resizeTimer.current !== null) return;
@@ -15,70 +28,73 @@ const ChatInput = ({message, hasInfoBox}) => {
         setBrowserWidth(window.innerWidth);
       }, 200);
     };
-  
     window.addEventListener("resize", handleResize);
-  
+    
     return () => {
-      window.addEventListener("resize", handleResize);
+      window.removeEventListener("resize", handleResize);
     };
   }, [browserWidth]);
 
   return (
     <StyledChatInput browserWidth={browserWidth}>
       <div className="chat-input-wrapper">
-        <div className="chat-input-core">
-          <textarea className="chat-input-text" id="chat-input-text" rows="6"></textarea>
-        </div>
-        <div className="chat-input-button">
-          <img src="inputIcons/InputButton.svg" style={{color: 'red', width: '36px', height: '36px'}}/>
+        <div className="chat-input">
+
+          {/* 메시지 입력 부분 */}
+          <textarea
+            className="chat-input-text"
+            id="chat-input-text"
+            value={message}
+            onChange={handleMessageChange}
+            rows="1"
+          />
+          
+          {/* 메시지 전송 부분 */}
+          <ChatInput_send_button
+            className="chat-input-button"
+            id="chat-input-button"
+            color={buttonActivate ? '#7B33FF' : '#EBEBEB'}
+          />
+
         </div>
       </div>
-
-
-      {/* <div style={{display: "flex", alignItems: "center", margin: "8px 14px"}}>
-        <div style={{padding: "10px 12px"}}>
-          <textarea className="chat-core" type="text" rows="1" style={{display: "flex"}}/>
-        </div>
-        <div >
-          <img src="inputIcons/InputButton.svg" style={{color: 'red', width: '36px', height: '36px'}}/>
-        </div>
-      </div> */}
     </StyledChatInput>
-  )
+  );
 };
 
 const StyledChatInput = styled.div`
-  .chat-input-wrapper {  
+  .chat-input-wrapper {
     display: flex;
-    align-items: center;
-    background-color: white;
-
+    border-top: 1px solid #EBEBEB;
     position: absolute;
     bottom: 0px;
-    width: 100%;
-
-    padding: 7px 14px;
-    border-top: 1px solid #EBEBEB;
-  }
-
-  .chat-input-core {
-    width: ${(props)=>props.browserWidth - 100}px;
-    position: relative;
+    width: ${(props)=>(props.browserWidth > 791) ? 791 : props.browserWidth}px;
   }
   
-  .chat-input-text {
+  .chat-input {
+    display: inline-block;
+    padding: 7px 14px;
     width: 100%;
-    height: auto;
-    border-radius: 10px;
-    padding: 10px 12px;
-    ${ Body14 }
+    margin: 0 5px;
   }
 
+  .chat-input-text {
+    width: ${(props)=>(props.browserWidth > 791) ? 791 - (28 + 38 + 36 + 10) : (props.browserWidth - 28 - 38 - 36 - 10)}px;
+    border-radius: 10px;
+    border: 1px solid #EBEBEB;
+    margin-right: 10px;
+    min-height: 36px;
+    // padding: 10px 12px;
+    top:50%;
+    ${ Body14 }
+  }
+  
   .chat-input-button {
     width: 36px;
     height: 36px;
-    margin-left: 36px;
-    position: relative;
+    margin: 0px;
+    padding: 0px;
+    bottom: 0;
   }
 `;
 
---
+++ src/svg/ChatInput_send_button.jsx
@@ -0,0 +1,19 @@
+const ChatInput_send_button = ({color}) => {
+  return (
+    <svg width="36" height="36" viewBox="0 0 36 36" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <g id="ETC Size" clip-path="url(#clip0_720_3741)">
+        <circle id="Ellipse 40" cx="18" cy="18" r="18" fill={color ? color : "#000000"} />
+        <path id="Vector" d="M18 26V10" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+        <path id="Vector_2" d="M10 18L18 10L26 18" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+      </g>
+      <defs>
+        <clipPath id="clip0_720_3741">
+          <rect width="36" height="36" fill="white"/>
+        </clipPath>
+      </defs>
+    </svg>
+
+  );
+};
+
+export default ChatInput_send_button;
