561967f1ba1ec0931a75af3042d41d0d92beebb3
sj-postlabs <seongjunpark@postlabs.io>
2023-07-05 10:51:35 +0900

    [23/07/05 10:51] feat : typography.js 파일 내에 Contents Header, Contents Body 추가

--- src/styles/typography.js
+++ src/styles/typography.js
@@ -43,7 +43,7 @@ export const Header12 = css`
 export const Header16 = css`
   font-size: 16px;
   font-family: "Pretendard Bold";
-  line-height: 23px;
+  line-height: 24px;
   font-weight: 700px;
   font-style: normal;
 `;
@@ -70,4 +70,150 @@ export const Header32 = css`
   line-height: 42px;
   font-weight: 700;
   font-style: normal;
+`;
+
+//----------------------------------------------
+
+export const ContentsBody20 = css`
+  font-size: 20px;
+  font-family: "Pretendard Regular";
+  line-height: 34.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: 0px;
+`;
+
+export const ContentsBody19 = css`
+  font-size: 19px;
+  font-family: "Pretendard Regular";
+  line-height: 32.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.1px;
+`;
+
+export const ContentsBody18 = css`
+  font-size: 18px;
+  font-family: "Pretendard Regular";
+  line-height: 30.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.15px;
+`;
+
+export const ContentsBody17 = css`
+  font-size: 17px;
+  font-family: "Pretendard Regular";
+  line-height: 28.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.2px;
+`;
+
+export const ContentsBody16 = css`
+  font-size: 16px;
+  font-family: "Pretendard Regular";
+  line-height: 26.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.25px;
+`;
+
+export const ContentsBody15 = css`
+  font-size: 15px;
+  font-family: "Pretendard Regular";
+  line-height: 24.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.35px;
+`;
+
+export const ContentsBody14 = css`
+  font-size: 14px;
+  font-family: "Pretendard Regular";
+  line-height: 22.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.4px;
+`;
+
+export const ContentsBody13 = css`
+  font-size: 13px;
+  font-family: "Pretendard Regular";
+  line-height: 20.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.45px;
+`;
+
+export const ContentsHeader20 = css`
+  font-size: 20px;
+  font-family: "Pretendard Bold";
+  line-height: 34.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: 0px;
+`;
+
+export const ContentsHeader19 = css`
+  font-size: 19px;
+  font-family: "Pretendard Bold";
+  line-height: 32.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.1px;
+`;
+
+export const ContentsHeader18 = css`
+  font-size: 18px;
+  font-family: "Pretendard Bold";
+  line-height: 30.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.15px;
+`;
+
+export const ContentsHeader17 = css`
+  font-size: 17px;
+  font-family: "Pretendard Bold";
+  line-height: 28.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.2px;
+`;
+
+export const ContentsHeader16 = css`
+  font-size: 16px;
+  font-family: "Pretendard Bold";
+  line-height: 26.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.25px;
+`;
+
+export const ContentsHeader15 = css`
+  font-size: 15px;
+  font-family: "Pretendard Bold";
+  line-height: 24.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.35px;
+`;
+
+export const ContentsHeader14 = css`
+  font-size: 14px;
+  font-family: "Pretendard Bold";
+  line-height: 22.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.4px;
+`;
+
+export const ContentsHeader13 = css`
+  font-size: 13px;
+  font-family: "Pretendard Bold";
+  line-height: 20.5px;
+  font-weight: 400;
+  font-style: normal;
+  letter-spacing: -0.45px;
 `;
